f3577efbb39b0fa9f84d82ac495f8e9ee327ba6d
avang <laevanotava@gmail.com>
2024-04-23 22:04:19 -0500

    Made class to load the character sprite when prompted

--- src/project.py
+++ src/project.py
@@ -1,6 +1,18 @@
 import pygame
 
 
+class LoadSprite():
+    def __init__(self, image):
+        self.sheet = image
+
+    def load_sheet(self, frame, width, height):
+        image = pygame.Surface((width, height))
+        image.blit(self.sheet, (0, 0), frame*width, 0, width, height)
+        black = (0, 0, 0)
+        # Creates Transparent Background
+        image.set_colorkey(black)
+        return image
+
 def main():
     pygame.init()
     pygame.display.set_caption("Underwater World")
